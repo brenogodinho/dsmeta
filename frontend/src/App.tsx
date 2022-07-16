92ebc3d5b9b9b956b5bba28bf6a289d46e03a9b2
Breno Godinho <breno.godinho@gmail.com>
2022-07-16 16:40:49 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
-import NotificationBotton from "./components/NotificationBotton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <h1>Olá mundo!</h1>
-      <NotificationBotton />
-      <NotificationBotton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
