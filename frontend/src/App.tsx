fbcd9e0c5a36cc90caa119c2f650b908d4049215
Breno Godinho <breno.godinho@gmail.com>
2022-07-16 13:14:35 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationBotton from "./components/NotificationBotton"
+
 function App() {
-  return(
-    <h1>Olá mundo!</h1>
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationBotton />
+      <NotificationBotton />
+    </>
   )
 }
 
---
+++ frontend/src/components/NotificationBotton/index.tsx
@@ -0,0 +1,13 @@
+import icon from "../../assets/img/notification-icon.svg"
+
+import "./styles.css"
+
+function NotificationBotton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={icon} alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationBotton
